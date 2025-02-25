a381f50218566e07dd81c686139cb802b3d8067f
Wendell Silva <wendellxj@hotmail.com>
2025-02-25 17:35:07 -0300

    dero 25022025

--- cpu/service-control-miner.sh
+++ cpu/service-control-miner.sh
@@ -20,7 +20,6 @@ export PATH="$PATH"
 # Log das variáveis de ambiente
 env >> "$ENV_LOGFILE"
 
-
 # Variáveis para o XMRig
 XMRIG_BINARY="/home/wendell/xdag/xmrig-4-xdag/xmrig-4-xdag"
 XMRIG_POOL="stratum.xdag.org:23656"
@@ -32,28 +31,22 @@ XMRIG_HTTP_TOKEN="auth"
 XMRIG_DONATE_LEVEL="1"
 CONFIG="/opt/xmrig/config.json"
 
-
-
 # Criar ou atualizar o arquivo de configuração do XMRig
-cat > "$XMRIG_CONFIG" <<EOL
-
-
+cat > "$CONFIG" <<EOL
 {
     "autosave": true,
     "cpu": {
         "enabled": true,
-        "huge-pages": true,                // Ativa o uso de páginas grandes para melhor desempenho
-        "hw-aes": true,                    // Ativa HW AES, se suportado pelo seu processador (melhora o desempenho em algoritmos que o utilizam)
-        "priority": 5,                     // Aumenta a prioridade do minerador (0 a 10, onde 10 é a mais alta)
-        "memory-pool": true,               // Ativa o pool de memória para melhorar a eficiência
-        "max-threads-hint": 100,           // Ajuste baseado no número de threads disponíveis
-        "asm": true,                       // Ativa as instruções ASM para melhor desempenho
-        "argon2-impl": null,               // Deixe como null a menos que você tenha uma implementação específica
-        "astrobwt-max-size": 550,          // Tamanho máximo do Astrobwt, mantenha ou ajuste conforme necessário
-        "astrobwt-avx2": true,             // Habilite AVX2 se seu CPU suportar, para melhorar o desempenho no Astrobwt
-        "cn/0": false,                     // Normalmente não é necessário ativar
-        "cn-lite/0": false,                // Normalmente não é necessário ativar
-        "1gb-pages": true                   // Ativa suporte a 1GB de páginas, se suportado pelo seu sistema
+        "huge-pages": true,
+        "hw-aes": true,
+        "priority": 5,
+        "memory-pool": true,
+        "max-threads-hint": 100,
+        "asm": true,
+        "argon2-impl": null,
+        "astrobwt-max-size": 550,
+        "astrobwt-avx2": true,
+        "1gb-pages": true
     },
     "http": {
         "enabled": true,
@@ -63,24 +56,22 @@ cat > "$XMRIG_CONFIG" <<EOL
         "restricted": false
     }
 }
-
-
 EOL
 
 # Garantir que o arquivo de configuração tenha permissões adequadas
-chmod 644 "$XMRIG_CONFIG"
+chmod 644 "$CONFIG"
 
 # Validar a existência do binário XMRig
 if [ -x "$XMRIG_BINARY" ]; then
     echo "$(date): Iniciando XMRig Miner..." >> "$XMRIG_LOGFILE"
-    "$XMRIG_BINARY" -o "$XMRIG_POOL" -u "$XMRIG_USER" -t "$XMRIG_THREADS" --algo="$XMRIG_ALGO" --donate-level="$XMRIG_DONATE_LEVEL" --config="$XMRIG_CONFIG" >> "$XMRIG_LOGFILE" 2>> "$ERROR_LOGFILE" &
+    "$XMRIG_BINARY" -o "$XMRIG_POOL" -u "$XMRIG_USER" -t "$XMRIG_THREADS" --algo="$XMRIG_ALGO" --donate-level="$XMRIG_DONATE_LEVEL" --config="$CONFIG" >> "$XMRIG_LOGFILE" 2>> "$ERROR_LOGFILE" &
 else
     echo "$(date): ERRO: Binário XMRig não encontrado ou sem permissões em $XMRIG_BINARY" >> "$ERROR_LOGFILE"
 fi
 
 # Aguardar um pouco
 sleep 2
-#teste
+
 # Variáveis para o Deroluna Miner
 DEROLUNA_BINARY="/home/wendell/dero_linux_amd64/deroluna-miner"
 
