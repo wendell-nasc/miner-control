a6abc1b835934d932418f7ac26c6b073879691ad
Wendell Silva <wendellxj@hotmail.com>
2025-02-25 13:35:16 -0300

    yada 25022025

--- cpu/service-control-miner.sh
+++ cpu/service-control-miner.sh
@@ -2,13 +2,15 @@
 
 # Definir arquivos de log
 XMRIG_LOGFILE="/var/log/start-xmrig-xdag_gustavo.log"
-YADA_LOGFILE="/var/log/YADA.log"
+DEROLUNA_LOGFILE="/var/log/start-deroluna-xdag_gustavo.log"
 ENV_LOGFILE="/var/log/start-env.log"
-ERROR_LOGFILE="/var/log/error.log"
+ERROR_LOGFILE="/var/log/start-miner-errors.log"
 
 # Garantir que os arquivos de log existam e tenham permissões adequadas
-for logfile in "$YADA_LOGFILE" "$XMRIG_LOGFILE" "$ENV_LOGFILE" "$ERROR_LOGFILE"; do
-    touch "$logfile"
+for logfile in "$XMRIG_LOGFILE" "$DEROLUNA_LOGFILE" "$ENV_LOGFILE" "$ERROR_LOGFILE"; do
+    if [ ! -f "$logfile" ]; then
+        touch "$logfile"
+    fi
     chmod 644 "$logfile"
 done
 
@@ -18,51 +20,24 @@ export PATH="$PATH"
 # Log das variáveis de ambiente
 env >> "$ENV_LOGFILE"
 
-# Função para calcular o número total de threads da CPU
-TOTAL_THREADS=$(nproc)
-
-# Divisão de threads com arredondamento correto para números ímpares
-THREADS_SRBMINER=$(( (TOTAL_THREADS * 2 + 2) / 3 ))  # 2/3 das threads
-THREADS_XMRIG=$((TOTAL_THREADS - THREADS_SRBMINER))  # 1/3 das threads
-
-echo "Configurando o SRBMiner para usar $THREADS_SRBMINER threads..." >> "$YADA_LOGFILE"
-echo "Configurando o XMRig para usar $THREADS_XMRIG threads..." >> "$XMRIG_LOGFILE"
-
-# Variáveis para armazenar os índices das threads
-THREADS_XMRIG_INDEXES=""
-THREADS_SRBMINER_INDEXES=""
-
-# Preencher os índices das threads para o SRBMiner (2/3 das threads)
-for ((i = 1; i <= THREADS_SRBMINER; i++)); do
-    if [ $i -gt 1 ]; then
-        THREADS_SRBMINER_INDEXES+=";"  # Adiciona ponto e vírgula para separar os valores
-    fi
-    THREADS_SRBMINER_INDEXES+="$i"  # Adiciona o índice
-done
-
-# Preencher os índices das threads para o XMRig (1/3 das threads)
-for ((i = THREADS_SRBMINER + 1; i <= TOTAL_THREADS; i++)); do
-    if [ $i -gt $((THREADS_SRBMINER + 1)) ]; then
-        THREADS_XMRIG_INDEXES+=";"  # Adiciona ponto e vírgula para separar os valores
-    fi
-    THREADS_XMRIG_INDEXES+="$i"  # Adiciona o índice
-done
-
-echo "Índices do SRBMiner: [$THREADS_SRBMINER_INDEXES]" >> "$YADA_LOGFILE"
 
 # Variáveis para o XMRig
-XMRIG_BINARY="/opt/xmrig/xmrig"
-XMRIG_POOL="new.yadaminers.pl:5555"
-XMRIG_USER="1BzY39AorUxyvCFgHzQ38yXZEbG9HmnfpS.$(hostname)"
-XMRIG_ALGO="rx/yada"
-XMRIG_THREADS=$(nproc)  # Número de threads baseado no cálculo acima
+XMRIG_BINARY="/home/wendell/xdag/xmrig-4-xdag/xmrig-4-xdag"
+XMRIG_POOL="stratum.xdag.org:23656"
+XMRIG_USER="Dzdbr5d8PVafQwvEkEwfNde7mFKNDaDSv.$(hostname)"
+XMRIG_ALGO="rx/xdag"
+XMRIG_THREADS=$(nproc)
 XMRIG_HTTP_PORT="37329"
 XMRIG_HTTP_TOKEN="auth"
-XMRIG_DONATE_LEVEL="1" # Nível de doação
-XMRIG_CONFIG="/opt/xmrig/config.json"
+XMRIG_DONATE_LEVEL="1"
+CONFIG="/opt/xmrig/config.json"
+
+
 
 # Criar ou atualizar o arquivo de configuração do XMRig
 cat > "$XMRIG_CONFIG" <<EOL
+
+
 {
     "autosave": true,
     "cpu": {
@@ -88,6 +63,8 @@ cat > "$XMRIG_CONFIG" <<EOL
         "restricted": false
     }
 }
+
+
 EOL
 
 # Garantir que o arquivo de configuração tenha permissões adequadas
@@ -96,53 +73,38 @@ chmod 644 "$XMRIG_CONFIG"
 # Validar a existência do binário XMRig
 if [ -x "$XMRIG_BINARY" ]; then
     echo "$(date): Iniciando XMRig Miner..." >> "$XMRIG_LOGFILE"
-    nice -n -20 "$XMRIG_BINARY" -o "$XMRIG_POOL" -u "$XMRIG_USER" -t "$XMRIG_THREADS" --algo="$XMRIG_ALGO" --donate-level="$XMRIG_DONATE_LEVEL" --config="$XMRIG_CONFIG" >> "$XMRIG_LOGFILE" 2>> "$ERROR_LOGFILE" &
+    "$XMRIG_BINARY" -o "$XMRIG_POOL" -u "$XMRIG_USER" -t "$XMRIG_THREADS" --algo="$XMRIG_ALGO" --donate-level="$XMRIG_DONATE_LEVEL" --config="$XMRIG_CONFIG" >> "$XMRIG_LOGFILE" 2>> "$ERROR_LOGFILE" &
 else
     echo "$(date): ERRO: Binário XMRig não encontrado ou sem permissões em $XMRIG_BINARY" >> "$ERROR_LOGFILE"
 fi
 
-# Variáveis para o minerador YADA
-YADA_BINARY="/opt/xmrig/xmrig"
-YADA_POOL="new.yadaminers.pl:5555"
-YADA_WALLET="1BzY39AorUxyvCFgHzQ38yXZEbG9HmnfpS"
-YADA_ALGO="rx/yada"
-YADA_DONATE_LEVEL="1" # Nível de doação
+# Aguardar um pouco
+sleep 2
+#teste
+# Variáveis para o Deroluna Miner
+DEROLUNA_BINARY="/home/wendell/dero_linux_amd64/deroluna-miner"
 
-# Verificar o IP atual
-CURRENT_IP=$(hostname -I | awk '{print $1}')
-TARGET_IP="192.168.1.199"
-
-# Se o IP corresponder ao alvo, executa o minerador SRBMiner e depois o XMRig
-if [ "$CURRENT_IP" == "$TARGET_IP" ]; then
-    echo "IP corresponde a $TARGET_IP. Executando minerador SRBMiner e XMRig..." >> "$YADA_LOGFILE"
-    
-    # Validar a existência do binário YADA
-    if [ -x "$YADA_BINARY" ]; then
-        echo "$(date): Iniciando YADA Miner..." >> "$YADA_LOGFILE"
-        nice -n -20 "$YADA_BINARY" -o "$YADA_POOL" -u "$YADA_WALLET.$(hostname)" --pass="x" -t "$TOTAL_THREADS" --algo="$YADA_ALGO" --donate-level="$YADA_DONATE_LEVEL" --config="$XMRIG_CONFIG" --nicehash=false --keepalive=true --rig-id=null --enabled=true >> "$YADA_LOGFILE" 2>> "$ERROR_LOGFILE" &
-    else
-        echo "$(date): ERRO: Binário YADA não encontrado ou sem permissões em $YADA_BINARY" >> "$ERROR_LOGFILE"
-    fi
-
-    sleep 5  # Esperar um pouco antes de iniciar o minerador XMRig
-
+# Definir a variável DEROLUNA_POOL com base na hora atual
+HORA_ATUAL=$(date +%H)
+if [ "$HORA_ATUAL" -ge 0 ] && [ "$HORA_ATUAL" -le 12 ]; then
+    DEROLUNA_POOL="dero-node-gustavogerman.mysrv.cloud:10100"
 else
-    # Caso o IP não corresponda, só executa o minerador YADA
-    echo "IP não corresponde. IP atual: $CURRENT_IP. Executando apenas o minerador YADA..." >> "$YADA_LOGFILE"
-    
-    # Validar a existência do binário YADA
-    if [ -x "$YADA_BINARY" ]; then
-        echo "$(date): Iniciando YADA Miner..." >> "$YADA_LOGFILE"
-        nice -n -20 "$YADA_BINARY" -o "$YADA_POOL" -u "$YADA_WALLET.$(hostname)" --pass="x" -t "$TOTAL_THREADS" --algo="$YADA_ALGO" --donate-level="$YADA_DONATE_LEVEL" --config="$XMRIG_CONFIG" --nicehash=false --keepalive=true --rig-id=null --enabled=true >> "$YADA_LOGFILE" 2>> "$ERROR_LOGFILE" &
-    else
-        echo "$(date): ERRO: Binário YADA não encontrado ou sem permissões em $YADA_BINARY" >> "$ERROR_LOGFILE"
-    fi
+    DEROLUNA_POOL="dero-node-gustavogerman.mysrv.cloud:10100"
+fi
 
-    sleep 5  # Esperar um pouco antes de iniciar o minerador YADA
+DEROLUNA_WALLET="dero1qy25zmq2kdzk644r9v89e5ukvkfahxecprduxcnh7zx0nndnl5y2vqqwpeu7z"
+DEROLUNA_THREADS=$(nproc)
+
+# Validar a existência do binário Deroluna
+if [ -x "$DEROLUNA_BINARY" ]; then
+    echo "$(date): Iniciando Deroluna Miner..." >> "$DEROLUNA_LOGFILE"
+    "$DEROLUNA_BINARY" --xmrig -d "$DEROLUNA_POOL" -w "$DEROLUNA_WALLET" -t "$DEROLUNA_THREADS" >> "$DEROLUNA_LOGFILE" 2>> "$ERROR_LOGFILE" &
+else
+    echo "$(date): ERRO: Binário Deroluna não encontrado ou sem permissões em $DEROLUNA_BINARY" >> "$ERROR_LOGFILE"
 fi
 
 # Esperar os processos em segundo plano
 wait
 
 # Mensagem final
-echo "$(date): Mineradores iniciados com sucesso." >> "$YADA_LOGFILE"
+echo "$(date): Mineradores iniciados com sucesso." | tee -a "$XMRIG_LOGFILE" "$DEROLUNA_LOGFILE"
