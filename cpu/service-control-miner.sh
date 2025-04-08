d46d93e25f34bf866b03077f5a328fcc0315f7ce
Wendell Silva <wendellxj@hotmail.com>
2025-04-07 21:08:47 -0300

    quntum 07042025

--- cpu/service-control-miner.sh
+++ cpu/service-control-miner.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Definir arquivos de log
+
 XMRIG_LOGFILE="/var/log/start-xmrig-xdag_gustavo.log"
-YADA_LOGFILE="/var/log/YADA.log"
+QRL_LOGFILE="/var/log/QRL.log"
 ENV_LOGFILE="/var/log/start-env.log"
-ERROR_LOGFILE="/var/log/error.log"
 
 # Garantir que os arquivos de log existam e tenham permissões adequadas
-for logfile in "$YADA_LOGFILE" "$XMRIG_LOGFILE" "$ENV_LOGFILE" "$ERROR_LOGFILE"; do
+for logfile in "$QRL_LOGFILE" "$XMRIG_LOGFILE" "$ENV_LOGFILE"; do
     touch "$logfile"
     chmod 644 "$logfile"
 done
@@ -25,7 +25,7 @@ TOTAL_THREADS=$(nproc)
 THREADS_SRBMINER=$(( (TOTAL_THREADS * 2 + 2) / 3 ))  # 2/3 das threads
 THREADS_XMRIG=$((TOTAL_THREADS - THREADS_SRBMINER))  # 1/3 das threads
 
-echo "Configurando o SRBMiner para usar $THREADS_SRBMINER threads..." >> "$YADA_LOGFILE"
+echo "Configurando o SRBMiner para usar $THREADS_SRBMINER threads..." >> "$QRL_LOGFILE"
 echo "Configurando o XMRig para usar $THREADS_XMRIG threads..." >> "$XMRIG_LOGFILE"
 
 # Variáveis para armazenar os índices das threads
@@ -48,101 +48,61 @@ for ((i = THREADS_SRBMINER + 1; i <= TOTAL_THREADS; i++)); do
     THREADS_XMRIG_INDEXES+="$i"  # Adiciona o índice
 done
 
-echo "Índices do SRBMiner: [$THREADS_SRBMINER_INDEXES]" >> "$YADA_LOGFILE"
+echo "Índices do SRBMiner: [$THREADS_SRBMINER_INDEXES]" >> "$QRL_LOGFILE"
 
-# Variáveis para o XMRig
-XMRIG_BINARY="/opt/xmrig/xmrig"
-XMRIG_POOL="new.yadaminers.pl:5555"
-XMRIG_USER="1KYJNwbFPHn6rFXZQr6XuibHRt9XGDErEZ.$(hostname)"
-XMRIG_ALGO="rx/yada"
-XMRIG_THREADS=$(nproc)  # Número de threads baseado no cálculo acima
-XMRIG_HTTP_PORT="37329"
-XMRIG_HTTP_TOKEN="auth"
-XMRIG_DONATE_LEVEL="1" # Nível de doação
-XMRIG_CONFIG="/opt/xmrig/config.json"
 
-# Criar ou atualizar o arquivo de configuração do XMRig
-cat > "$XMRIG_CONFIG" <<EOL
-{
-    "autosave": true,
-    "cpu": {
-        "enabled": true,
-        "huge-pages": true,                // Ativa o uso de páginas grandes para melhor desempenho
-        "hw-aes": true,                    // Ativa HW AES, se suportado pelo seu processador (melhora o desempenho em algoritmos que o utilizam)
-        "priority": 5,                     // Aumenta a prioridade do minerador (0 a 10, onde 10 é a mais alta)
-        "memory-pool": true,               // Ativa o pool de memória para melhorar a eficiência
-        "max-threads-hint": 100,           // Ajuste baseado no número de threads disponíveis
-        "asm": true,                       // Ativa as instruções ASM para melhor desempenho
-        "argon2-impl": null,               // Deixe como null a menos que você tenha uma implementação específica
-        "astrobwt-max-size": 550,          // Tamanho máximo do Astrobwt, mantenha ou ajuste conforme necessário
-        "astrobwt-avx2": true,             // Habilite AVX2 se seu CPU suportar, para melhorar o desempenho no Astrobwt
-        "cn/0": false,                     // Normalmente não é necessário ativar
-        "cn-lite/0": false,                // Normalmente não é necessário ativar
-        "1gb-pages": true                   // Ativa suporte a 1GB de páginas, se suportado pelo seu sistema
-    },
-    "http": {
-        "enabled": true,
-        "host": "127.0.0.1",
-        "port": 37329,
-        "access-token": "auth",
-        "restricted": false
-    }
-}
-EOL
+# Variáveis para o minerador QRL
+QRL_BINARY="/home/wendell/SRBMiner/SRBMiner-Multi-2-6-5/SRBMiner-MULTI"
+QRL_POOL="br.qrl.herominers.com:1166"
+QRL_WALLET="Q0105004c5fde633090f2cfa0dcd301547b8a6a39429f56c5a01705cc9359def9aee34fcdc9d18e"
+QRL_ALGO="randomx"
 
-# Garantir que o arquivo de configuração tenha permissões adequadas
-chmod 644 "$XMRIG_CONFIG"
 
-# Validar a existência do binário XMRig
-if [ -x "$XMRIG_BINARY" ]; then
-    echo "$(date): Iniciando XMRig Miner..." >> "$XMRIG_LOGFILE"
-    nice -n -20 "$XMRIG_BINARY" -o "$XMRIG_POOL" -u "$XMRIG_USER" -t "$XMRIG_THREADS" --algo="$XMRIG_ALGO" --donate-level="$XMRIG_DONATE_LEVEL" --config="$XMRIG_CONFIG" >> "$XMRIG_LOGFILE" 2>> "$ERROR_LOGFILE" &
+# Verificar se o minerador existe, caso contrário, baixar e extrair
+if [ ! -f "$QRL_BINARY" ]; then
+    echo "Minerador não encontrado. Baixando e extraindo..." >> "$QRL_LOGFILE"
+    
+    # Criar diretório e navegar até ele
+    mkdir -p /home/wendell/SRBMiner
+    cd /home/wendell/SRBMiner || { echo "Falha ao acessar o diretório"; exit 1; }
+
+    # Baixar e extrair o minerador
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.5/SRBMiner-Multi-2-6-5-Linux.tar.gz || { echo "Falha ao baixar o minerador"; exit 1; }
+    tar -xvf SRBMiner-Multi-2-6-5-Linux.tar.gz || { echo "Falha ao extrair o minerador"; exit 1; }
+    echo "Minerador baixado e extraído." >> "$QRL_LOGFILE"
 else
-    echo "$(date): ERRO: Binário XMRig não encontrado ou sem permissões em $XMRIG_BINARY" >> "$ERROR_LOGFILE"
+    echo "Minerador já encontrado. Prosseguindo..." >> "$QRL_LOGFILE"
 fi
 
-# Variáveis para o minerador YADA
-YADA_BINARY="/opt/xmrig/xmrig"
-YADA_POOL="new.yadaminers.pl:5555"
-YADA_WALLET="1BzY39AorUxyvCFgHzQ38yXZEbG9HmnfpS"
-YADA_ALGO="rx/yada"
-YADA_DONATE_LEVEL="1" # Nível de doação
 
 # Verificar o IP atual
 CURRENT_IP=$(hostname -I | awk '{print $1}')
 TARGET_IP="192.168.1.199"
 
+
 # Se o IP corresponder ao alvo, executa o minerador SRBMiner e depois o XMRig
 if [ "$CURRENT_IP" == "$TARGET_IP" ]; then
-    echo "IP corresponde a $TARGET_IP. Executando minerador SRBMiner e XMRig..." >> "$YADA_LOGFILE"
+    echo "IP corresponde a $TARGET_IP. Executando minerador SRBMiner e XMRig..." >> "$QRL_LOGFILE"
     
-    # Validar a existência do binário YADA
-    if [ -x "$YADA_BINARY" ]; then
-        echo "$(date): Iniciando YADA Miner..." >> "$YADA_LOGFILE"
-        nice -n -20 "$YADA_BINARY" -o "$YADA_POOL" -u "$YADA_WALLET.$(hostname)" --pass="x" -t "$TOTAL_THREADS" --algo="$YADA_ALGO" --donate-level="$YADA_DONATE_LEVEL" --config="$XMRIG_CONFIG" --nicehash=false --keepalive=true --rig-id=null --enabled=true >> "$YADA_LOGFILE" 2>> "$ERROR_LOGFILE" &
-    else
-        echo "$(date): ERRO: Binário YADA não encontrado ou sem permissões em $YADA_BINARY" >> "$ERROR_LOGFILE"
-    fi
-
+    # Iniciar o minerador SRBMiner
+    nice -n -20 "$QRL_BINARY" --disable-gpu --algorithm "$QRL_ALGO" --pool "$QRL_POOL" --wallet "$QRL_WALLET" --password "$(hostname)" --cpu-threads $TOTAL_THREADS --keepalive true --randomx-use-1gb-pages --cpu-threads-priority 5 & >> "$QRL_LOGFILE" 2>> /var/log/start-deroluna-errors.log &
     sleep 5  # Esperar um pouco antes de iniciar o minerador XMRig
 
 else
-    # Caso o IP não corresponda, só executa o minerador YADA
-    echo "IP não corresponde. IP atual: $CURRENT_IP. Executando apenas o minerador YADA..." >> "$YADA_LOGFILE"
+    # Caso o IP não corresponda, só executa o minerador XMRig
+    echo "IP não corresponde. IP atual: $CURRENT_IP. Executando apenas o minerador XMRig..." >> "$QRL_LOGFILE"
     
-    # Validar a existência do binário YADA
-    if [ -x "$YADA_BINARY" ]; then
-        echo "$(date): Iniciando YADA Miner..." >> "$YADA_LOGFILE"
-        nice -n -20 "$YADA_BINARY" -o "$YADA_POOL" -u "$YADA_WALLET.$(hostname)" --pass="x" -t "$TOTAL_THREADS" --algo="$YADA_ALGO" --donate-level="$YADA_DONATE_LEVEL" --config="$XMRIG_CONFIG" --nicehash=false --keepalive=true --rig-id=null --enabled=true >> "$YADA_LOGFILE" 2>> "$ERROR_LOGFILE" &
-    else
-        echo "$(date): ERRO: Binário YADA não encontrado ou sem permissões em $YADA_BINARY" >> "$ERROR_LOGFILE"
-    fi
+    # Iniciar o minerador SRBMiner
+    nice -n -20 "$QRL_BINARY" --disable-gpu --algorithm "$QRL_ALGO" --pool "$QRL_POOL" --wallet "$QRL_WALLET" --password "$(hostname)" --cpu-threads $TOTAL_THREADS --keepalive true --randomx-use-1gb-pages --cpu-threads-priority 5 & >> "$QRL_LOGFILE" 2>> /var/log/start-deroluna-errors.log &
+    
+    sleep 5
+    
 
-    sleep 5  # Esperar um pouco antes de iniciar o minerador YADA
 fi
+
 
 # Esperar os processos em segundo plano
 wait
 
 # Mensagem final
-echo "$(date): Mineradores iniciados com sucesso." >> "$YADA_LOGFILE"
+echo "$(date): Mineradores iniciados com sucesso."
